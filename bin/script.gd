efef14d7be0f008680d7659f5574a4291c028686
Paweł Fertyk <pfertyk@pfertyk.me>
2023-04-04 23:06:41 +0200

    Print dummy output and save to results.json

--- bin/script.gd
+++ bin/script.gd
@@ -2,5 +2,13 @@ extends SceneTree
 
 
 func _init():
-	print("Hello, GDScript Test Runner!")
-	quit()
+	# TODO: read and use 3 args
+	var args = OS.get_cmdline_user_args()
+	
+	var results = {
+		"version": 2,
+		"status": "pass",
+	}
+	var pretty_results = JSON.stringify(results, "  ", false)
+	print(pretty_results)
+	quit(1)
