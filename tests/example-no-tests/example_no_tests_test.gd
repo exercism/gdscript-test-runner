bee11ad99c09915a68863e1892822c3d286442b3
Paweł Fertyk <pfertyk@pfertyk.me>
2024-03-24 20:26:53 +0100

    Detect lack of tests
    Currently, if the test file is invalid (e.g. mixes tab and space
    indentation), no tests will be executed by the runner. However, the
    situation is not currently treated as an error, resulting in a green
    check in GitHub actions. Because of this, after adding each exercise,
    the logs have to be manually validated to confirm that the corresponding
    tests were correctly executed.
    
    This PR will make the test suite fail if no tests were executed.

---
+++ tests/example-no-tests/example_no_tests_test.gd
@@ -0,0 +1,6 @@
+func invalid_test_1_and_2_equals_3(solution_script):
+	return [3, solution_script.add_2_numbers(1, 2)]
+
+
+func invalid_test_10_and_20_equals_30(solution_script):
+	return [30, solution_script.add_2_numbers(10, 20)]
