b8945b9dbf95c29bbdfd84e97d6c2482689c180c
Kevin Bloch <kev@codingthat.com>
2025-06-28 19:30:02 +0200

    Fixes thanks to shellcheck

--- bin/test-local-gdscript-solution.sh
+++ bin/test-local-gdscript-solution.sh
@@ -12,25 +12,24 @@ missing_gd_msg="Normally, the exercise subdirectory created by the exercism down
 general_help_msg="Please see https://exercism.org/docs/tracks/gdscript/installation for details."
 if [ ! -f "${slug//-/_}_test.gd" ]; then
     echo "Missing test file: ${slug//-/_}_test.gd"
-    echo $missing_gd_msg
-    echo $general_help_msg
+    echo "$missing_gd_msg"
+    echo "$general_help_msg"
     exit 1
 fi
 if [ ! -f "${slug//-/_}.gd" ]; then
     echo "Missing solution file: ${slug//-/_}.gd"
-    echo $missing_gd_msg
-    echo $general_help_msg
+    echo "$missing_gd_msg"
+    echo "$general_help_msg"
     exit 1
 fi
 if [ ! -f "/opt/exercism/gdscript/test-runner/bin/run.sh" ]; then
     echo "Missing test runner file: /opt/exercism/gdscript/test-runner/bin/run.sh"
-    echo $general_help_msg
+    echo "$general_help_msg"
     exit 1
 fi
 
 solution_dir="$(pwd)"
 output_dir="${solution_dir}/.test-output"
-results_file="${output_dir}/results.json"
 mkdir -p "${output_dir}"
 
 (cd /opt/exercism/gdscript/test-runner && godot --headless -s bin/test_runner.gd -- "${slug}" "${solution_dir}") || {
